7be2f590367d956be64a5f9be550cd9588ecaad3
henric <software@henric.org>
2012-12-20 00:33:42 -0700

    Make the BufferingManager more reluctant to stop supplying data to the MediaElement. Include the buffer size (in bytes) when deciding that enough has been buffered.

--- Source/Libraries/SM.Media/BufferingManager.cs
+++ Source/Libraries/SM.Media/BufferingManager.cs
@@ -1,21 +1,21 @@
-//-----------------------------------------------------------------------
-// <copyright file="BufferingManager.cs" company="Henric Jungheim">
-// Copyright (c) 2012.
-// <author>Henric Jungheim</author>
-// </copyright>
-//-----------------------------------------------------------------------
-// Copyright (c) 2012 Henric Jungheim <software@henric.org> 
-//
+// -----------------------------------------------------------------------
+//  <copyright file="BufferingManager.cs" company="Henric Jungheim">
+//  Copyright (c) 2012.
+//  <author>Henric Jungheim</author>
+//  </copyright>
+// -----------------------------------------------------------------------
+// Copyright (c) 2012 Henric Jungheim <software@henric.org>
+// 
 // Permission is hereby granted, free of charge, to any person obtaining a
 // copy of this software and associated documentation files (the "Software"),
 // to deal in the Software without restriction, including without limitation
 // the rights to use, copy, modify, merge, publish, distribute, sublicense,
 // and/or sell copies of the Software, and to permit persons to whom the
 // Software is furnished to do so, subject to the following conditions:
-//
+// 
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-//
+// 
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
@@ -33,12 +33,15 @@ namespace SM.Media
 {
     class BufferingManager : IBufferingManager
     {
-        static readonly TimeSpan BufferEnableThreshold = TimeSpan.FromSeconds(2);
-        static readonly TimeSpan BufferThreshold = TimeSpan.FromSeconds(4);
-        static readonly TimeSpan BufferDisableThreshold = TimeSpan.FromSeconds(8);
+        const int BufferSizeMaximum = 300 * 1024;
+        const int BufferSizeStopBuffering = 150 * 1024;
+        const int BufferSizeStartBuffering = 50 * 1024;
+        static readonly TimeSpan BufferDurationEnableThreshold = TimeSpan.FromSeconds(2);
+        static readonly TimeSpan BufferDurationThreshold = TimeSpan.FromSeconds(4);
+        static readonly TimeSpan BufferDurationDisableThreshold = TimeSpan.FromSeconds(8);
         static readonly TimeSpan BufferStatusUpdatePeriod = TimeSpan.FromMilliseconds(250);
-        readonly IQueueThrottling _queueThrottling;
         readonly object _lock = new object();
+        readonly IQueueThrottling _queueThrottling;
         readonly List<Queue> _queues = new List<Queue>();
         readonly Action<double> _reportBuffering;
         bool _blockReads;
@@ -50,181 +53,6 @@ namespace SM.Media
         {
             _queueThrottling = queueThrottling;
             _reportBuffering = reportBuffering;
-        }
-
-        void Report(Action<int, TimeSpan> update, int size, TimeSpan timestamp)
-        {
-            var wasBlock = _blockReads;
-
-            lock (_lock)
-            {
-                update(size, timestamp);
-
-                UnlockedReport();
-
-                //Debug.WriteLine("Report Sample({0}, {1}) blockReads {2} => {3} ({4})", size, timestamp, wasBlock, _blockReads, DateTimeOffset.Now);
-            }
-        }
-
-        void UnlockedReport()
-        {
-            var shouldBlock = UpdateState();
-
-            if (shouldBlock != _blockReads)
-            {
-                _blockReads = shouldBlock;
-
-                HandleStateChange();
-            }
-        }
-
-        void ReportExhaustion(Action update)
-        {
-            Debug.WriteLine("BufferingManager.ReportExhaustion(...)");
-
-            lock (_lock)
-            {
-                update();
-
-                UnlockedStartBuffering();
-
-                if (!_blockReads)
-                    return;
-
-                _blockReads = false;
-
-                HandleStateChange();
-            }
-        }
-
-        void ReportDone(Action update)
-        {
-            lock (_lock)
-            {
-                update();
-
-                UnlockedReport();
-            }
-        }
-
-        void UnlockedStartBuffering()
-        {
-#pragma warning disable 0420
-            var wasBuffering = Interlocked.Exchange(ref _isBuffering, 1);
-#pragma warning restore 0420
-
-            if (0 != wasBuffering)
-                return;
-
-            if (null != _reportBuffering)
-                _reportBuffering(0);
-        }
-
-        bool UpdateState()
-        {
-            var newest = TimeSpan.MaxValue;
-            var oldest = TimeSpan.MinValue;
-            var lowest = int.MaxValue;
-
-            var validData = false;
-
-            for (var i = 0; i < _queues.Count; ++i)
-            {
-                var queue = _queues[i];
-
-                if (!queue.IsValid)
-                    continue;
-
-                validData = true;
-
-                var count = queue.PacketCount;
-
-                if (count < lowest)
-                {
-                    if (count < 2)
-                    {
-                        // We are ending or out of data, so don't suspend anything.
-                        return false;
-                    }
-
-                    lowest = count;
-                }
-
-                var newTime = queue.Newest;
-
-                if (newTime < newest)
-                    newest = newTime;
-
-                var oldTime = queue.Oldest;
-
-                if (oldTime > oldest)
-                    oldest = oldTime;
-            }
-
-            if (_playbackPosition.HasValue)
-                oldest = _playbackPosition.Value;
-
-            var timestampDifference = validData ? newest - oldest : TimeSpan.MaxValue;
-
-            if (0 != _isBuffering)
-                UpdateBuffering(timestampDifference);
-
-            if (!validData)
-                return false;
-
-            if (timestampDifference < BufferEnableThreshold)
-                return false;
-
-            if (timestampDifference > BufferDisableThreshold)
-                return true;
-
-            return false;
-        }
-
-        void UpdateBuffering(TimeSpan timestampDifference)
-        {
-            if (timestampDifference >= BufferThreshold)
-            {
-#pragma warning disable 0420
-                Interlocked.Exchange(ref _isBuffering, 0);
-#pragma warning restore 0420
-
-                Debug.WriteLine("BufferingManager.UpdateBuffering done buffering");
-
-                if (null != _reportBuffering)
-                    _reportBuffering(1);
-            }
-            else
-            {
-                var now = DateTime.UtcNow;
-
-                var elapsed = now - _bufferStatusTimeUtc;
-
-                if (elapsed >= BufferStatusUpdatePeriod)
-                {
-                    _bufferStatusTimeUtc = now;
-
-                    var bufferingStatus = timestampDifference.Ticks / (double)BufferThreshold.Ticks;
-
-                    Debug.WriteLine("BufferingManager.UpdateBuffering: {0}%", bufferingStatus * 100);
-
-                    if (null != _reportBuffering)
-                        _reportBuffering(bufferingStatus);
-                }
-            }
-        }
-
-        void HandleStateChange()
-        {
-            var er = _queueThrottling;
-
-            if (null == er)
-                return;
-
-            if (_blockReads)
-                er.Pause();
-            else
-                er.Resume();
         }
 
         #region IBufferingManager Members
@@ -315,15 +143,215 @@ namespace SM.Media
 
         #endregion
 
+        void Report(Action<int, TimeSpan> update, int size, TimeSpan timestamp)
+        {
+            var wasBlock = _blockReads;
+
+            lock (_lock)
+            {
+                update(size, timestamp);
+
+                UnlockedReport();
+
+                //Debug.WriteLine("Report Sample({0}, {1}) blockReads {2} => {3} ({4})", size, timestamp, wasBlock, _blockReads, DateTimeOffset.Now);
+            }
+        }
+
+        void UnlockedReport()
+        {
+            var shouldBlock = UpdateState();
+
+            if (shouldBlock != _blockReads)
+            {
+                _blockReads = shouldBlock;
+
+                HandleStateChange();
+            }
+        }
+
+        void ReportExhaustion(Action update)
+        {
+            Debug.WriteLine("BufferingManager.ReportExhaustion(...)");
+
+            lock (_lock)
+            {
+                update();
+
+                UnlockedReport();
+            }
+        }
+
+        void ReportDone(Action update)
+        {
+            lock (_lock)
+            {
+                update();
+
+                UnlockedReport();
+            }
+        }
+
+        void UnlockedStartBuffering()
+        {
+#pragma warning disable 0420
+            var wasBuffering = Interlocked.Exchange(ref _isBuffering, 1);
+#pragma warning restore 0420
+
+            if (0 != wasBuffering)
+                return;
+
+            if (null != _reportBuffering)
+                _reportBuffering(0);
+        }
+
+        bool UpdateState()
+        {
+            var newest = TimeSpan.MinValue;
+            var oldest = TimeSpan.MaxValue;
+            var lowestCount = int.MaxValue;
+            var highestCount = int.MinValue;
+            var totalBuffered = 0;
+
+            var validData = false;
+
+            for (var i = 0; i < _queues.Count; ++i)
+            {
+                var queue = _queues[i];
+
+                if (!queue.IsValid)
+                    continue;
+
+                totalBuffered += queue.Size;
+
+                validData = true;
+
+                var count = queue.PacketCount;
+
+                if (count < lowestCount)
+                    lowestCount = count;
+
+                if (count > highestCount)
+                    highestCount = count;
+
+                var newTime = queue.Newest;
+
+                if (newTime > newest)
+                    newest = newTime;
+
+                var oldTime = queue.Oldest;
+
+                if (oldTime < oldest)
+                    oldest = oldTime;
+            }
+
+            if (_playbackPosition.HasValue)
+            {
+                var time = _playbackPosition.Value;
+
+                if (time < newest && time > oldest)
+                    oldest = time;
+            }
+
+            var timestampDifference = validData ? newest - oldest : TimeSpan.MaxValue;
+
+            Debug.Assert(timestampDifference >= TimeSpan.Zero);
+
+            if (0 != _isBuffering)
+            {
+                UpdateBuffering(timestampDifference, totalBuffered);
+
+                if (0 != _isBuffering)
+                    return false;
+            }
+            else
+            {
+                //if (validData && 0 == lowestCount && timestampDifference < BufferDurationEnableThreshold && totalBuffered < BufferSizeStartBuffering)
+                if (validData && 0 == highestCount)
+                {
+                    Debug.WriteLine("BufferingManager.UpdateState start buffering: {0} duration, {1} size, {2} memory", timestampDifference, totalBuffered, GC.GetTotalMemory(false));
+
+                    UnlockedStartBuffering();
+
+                    return false;
+                }
+            }
+
+            if (!validData)
+                return false;
+
+            if (totalBuffered > BufferSizeMaximum)
+                return true;
+
+            if (timestampDifference < BufferDurationEnableThreshold)
+                return false;
+
+            if (timestampDifference > BufferDurationDisableThreshold)
+                return true;
+
+            return false;
+        }
+
+        void UpdateBuffering(TimeSpan timestampDifference, int bufferSize)
+        {
+            if ((timestampDifference >= BufferDurationThreshold && bufferSize >= BufferSizeStopBuffering) || bufferSize >= BufferSizeMaximum)
+            {
+#pragma warning disable 0420
+                Interlocked.Exchange(ref _isBuffering, 0);
+#pragma warning restore 0420
+
+                Debug.WriteLine("BufferingManager.UpdateBuffering done buffering: {0} duration, {1} size, {2} memory", timestampDifference, bufferSize, GC.GetTotalMemory(false));
+
+                if (null != _reportBuffering)
+                    _reportBuffering(1);
+            }
+            else
+            {
+                var now = DateTime.UtcNow;
+
+                var elapsed = now - _bufferStatusTimeUtc;
+
+                if (elapsed >= BufferStatusUpdatePeriod)
+                {
+                    _bufferStatusTimeUtc = now;
+
+                    var bufferingStatus1 = Math.Max(0, timestampDifference.Ticks / (double)BufferDurationThreshold.Ticks);
+                    var bufferingStatus2 = bufferSize / (double)BufferSizeStopBuffering;
+                    var bufferingStatus3 = bufferSize / (double)BufferSizeMaximum;
+
+                    var bufferingStatus = Math.Max(Math.Min(bufferingStatus1, bufferingStatus2), bufferingStatus3);
+
+                    Debug.WriteLine("BufferingManager.UpdateBuffering: {0}%, {1} duration, {2} size, {3} memory", bufferingStatus * 100, timestampDifference, bufferSize, GC.GetTotalMemory(false));
+
+                    if (null != _reportBuffering)
+                        _reportBuffering(bufferingStatus);
+                }
+            }
+        }
+
+        void HandleStateChange()
+        {
+            var er = _queueThrottling;
+
+            if (null == er)
+                return;
+
+            if (_blockReads)
+                er.Pause();
+            else
+                er.Resume();
+        }
+
+        #region Nested type: Queue
+
         class Queue : IBufferingQueue
         {
             readonly BufferingManager _bufferingManager;
             int _bufferSize;
             bool _firstPacket;
+            bool _isDone;
             TimeSpan _newestPacket;
             TimeSpan _oldestPacket;
             int _packetCount;
-            bool _isDone;
 
             public Queue(BufferingManager bufferingManager)
             {
@@ -355,6 +383,8 @@ namespace SM.Media
                 get { return _bufferSize; }
             }
 
+            #region IBufferingQueue Members
+
             public void ReportEnqueue(int size, TimeSpan timestamp)
             {
                 _bufferingManager.Report(Enqueue, size, timestamp);
@@ -374,6 +404,8 @@ namespace SM.Media
             {
                 _bufferingManager.ReportDone(Done);
             }
+
+            #endregion
 
             void Done()
             {
@@ -414,5 +446,7 @@ namespace SM.Media
                 }
             }
         }
+
+        #endregion
     }
 }

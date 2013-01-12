c1041740cb14a2e18a58211724cfd23eafc4696c
henric <software@henric.org>
2013-01-12 10:59:12 -0700

    Give the playlist an expiration time.  This should reduce the chance of stream discontinuities from excessive buffering.

--- Source/Libraries/SM.Media/Playlists/PlaylistSegmentManager.cs
+++ Source/Libraries/SM.Media/Playlists/PlaylistSegmentManager.cs
@@ -26,6 +26,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Threading;
@@ -48,6 +49,7 @@ namespace SM.Media.Playlists
         int _segmentIndex;
         SubStreamSegment[] _segments;
         ICachedWebRequest _subPlaylistRequest;
+        int _segmentsExpiration;
 
         public PlaylistSegmentManager(Func<Uri, ICachedWebRequest> webRequestFactory, ISubProgram program)
             : this(webRequestFactory, program, CancellationToken.None)
@@ -94,9 +96,7 @@ namespace SM.Media.Playlists
             for (; ; )
             {
                 if (_isDynamicPlayist)
-                {
                     await CheckReload().ConfigureAwait(false);
-                }
 
                 lock (_segmentLock)
                 {
@@ -186,13 +186,13 @@ namespace SM.Media.Playlists
                 // miss segments.  To get that working properly, we must
                 // adjust the sample timestamps since otherwise there will
                 // be a discontinuity for MediaElement to choke on.
-                segmentIndex = 0; // _segments.Length / 4 - 1;
+                segmentIndex = _segments.Length / 3 - 1;
 
-                //if (segmentIndex + 4 >= _segments.Length)
-                //    segmentIndex = _segments.Length - 5;
+                if (segmentIndex + 4 >= _segments.Length)
+                    segmentIndex = _segments.Length - 5;
 
-                //if (segmentIndex < -1)
-                //    segmentIndex = -1;
+                if (segmentIndex < -1)
+                    segmentIndex = -1;
             }
 
             _segmentIndex = segmentIndex;
@@ -202,7 +202,7 @@ namespace SM.Media.Playlists
         {
             lock (_segmentLock)
             {
-                if (null != _segments && _segmentIndex + 2 < _segments.Length)
+                if (null != _segments && _segmentIndex + 2 < _segments.Length && (!_isDynamicPlayist || _segmentsExpiration - Environment.TickCount > 0))
                     return TplTaskExtensions.CompletedTask;
 
                 if (null != _reReader)
@@ -290,18 +290,48 @@ namespace SM.Media.Playlists
                 {
                     var url = oldSegments[oldSegmentIndex].Url;
 
-                    for (var i = 0; i < _segments.Length; ++i)
-                    {
-                        if (url == _segments[i].Url)
-                        {
-                            _segmentIndex = i;
-                            break;
-                        }
-                    }
+                    var index = FindIndexByUrl(url);
+
+                    if (index > 0)
+                        _segmentIndex = index;
+                    else
+                        Debug.WriteLine("PlaylistSegmentManager.ReadSubList: playlist discontinuity {0} does not contain {1}", parser.BaseUrl, url);
                 }
+
+                if (isDynamicPlayist)
+                {
+                    var duration = GetDuration(_segmentIndex > 0 ? segments.Skip(_segmentIndex) : _segments);
+
+                    // We use the system uptime rather than DateTime.UtcNow to
+                    // avoid grief if there is a step in the system time.
+                    // TODO: Make sure the TickCount doesn't get confused by steps in the system time.
+                    var segmentsExpiration = Environment.TickCount;
+
+                    if (duration.HasValue)
+                        segmentsExpiration += (int)(duration.Value.TotalMilliseconds * 0.90);
+                    else
+                        segmentsExpiration += 5000 * segments.Length;
+
+                    _segmentsExpiration = segmentsExpiration;
+                }
+
+                Debug.WriteLine("PlaylistSegmentManager.ReadSubList: playlist {0} loaded with {1} entries. index: {2} dynamic: {3} expires: {4}",
+                    parser.BaseUrl, _segments.Length, _segmentIndex, isDynamicPlayist,
+                    isDynamicPlayist ? TimeSpan.FromMilliseconds( _segmentsExpiration - Environment.TickCount) : TimeSpan.Zero);
 
                 _reReader = null;
             }
+        }
+
+        int FindIndexByUrl(Uri url)
+        {
+            for (var i = 0; i < _segments.Length; ++i)
+            {
+                if (url == _segments[i].Url)
+                    return i;
+            }
+
+            return -1;
         }
 
         static TimeSpan? GetDuration(IEnumerable<SubStreamSegment> segments)

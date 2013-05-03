daeb69886223162cee7685a8c31b5f039542f7d8
henric <software@henric.org>
2013-05-03 00:13:29 -0700

    16-bit CRC lookup tables only need ushort.

--- Source/Console/ComputeCrcTable/Program.cs
+++ Source/Console/ComputeCrcTable/Program.cs
@@ -1,21 +1,21 @@
-//-----------------------------------------------------------------------
-// <copyright file="Program.cs" company="Henric Jungheim">
-// Copyright (c) 2012.
-// <author>Henric Jungheim</author>
-// </copyright>
-//-----------------------------------------------------------------------
-// Copyright (c) 2012 Henric Jungheim <software@henric.org> 
-//
+// -----------------------------------------------------------------------
+//  <copyright file="Program.cs" company="Henric Jungheim">
+//  Copyright (c) 2012, 2013.
+//  <author>Henric Jungheim</author>
+//  </copyright>
+// -----------------------------------------------------------------------
+// Copyright (c) 2012, 2013 Henric Jungheim <software@henric.org>
+// 
 // Permission is hereby granted, free of charge, to any person obtaining a
 // copy of this software and associated documentation files (the "Software"),
 // to deal in the Software without restriction, including without limitation
 // the rights to use, copy, modify, merge, publish, distribute, sublicense,
 // and/or sell copies of the Software, and to permit persons to whom the
 // Software is furnished to do so, subject to the following conditions:
-//
+// 
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-//
+// 
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
@@ -121,7 +121,7 @@ namespace ComputeCrcTable
 
             Console.WriteLine("// CRC16 Lookup for 0x{0:x4}", poly);
             Console.WriteLine();
-            Console.WriteLine("static readonly uint[] Crc16Table_0x{0:x4} =", poly);
+            Console.WriteLine("static readonly ushort[] Crc16Table_0x{0:x4} =", poly);
             Console.WriteLine("{");
             Console.Write("    ");
             var count = 0;
@@ -131,7 +131,7 @@ namespace ComputeCrcTable
 
                 ++count;
 
-                if (0 == (count & 0x03))
+                if (0 == (count & 0x07))
                 {
                     count = 0;
                     Console.WriteLine();

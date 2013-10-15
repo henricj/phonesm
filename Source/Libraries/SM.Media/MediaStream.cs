10ffc7df558c4554c6e86fdb77dc180ac6576d99
henric <software@henric.org>
2013-10-15 08:26:03 -0700

    Rename MediaStream's _streamBuffer to _streamSource.

--- Source/Libraries/SM.Media/MediaStream.cs
+++ Source/Libraries/SM.Media/MediaStream.cs
@@ -1,21 +1,21 @@
-//-----------------------------------------------------------------------
-// <copyright file="MediaStream.cs" company="Henric Jungheim">
-// Copyright (c) 2012.
-// <author>Henric Jungheim</author>
-// </copyright>
-//-----------------------------------------------------------------------
-// Copyright (c) 2012 Henric Jungheim <software@henric.org> 
-//
+// -----------------------------------------------------------------------
+//  <copyright file="MediaStream.cs" company="Henric Jungheim">
+//  Copyright (c) 2012, 2013.
+//  <author>Henric Jungheim</author>
+//  </copyright>
+// -----------------------------------------------------------------------
+// Copyright (c) 2012, 2013 Henric Jungheim <software@henric.org>
+// 
 // Permission is hereby granted, free of charge, to any person obtaining a
 // copy of this software and associated documentation files (the "Software"),
 // to deal in the Software without restriction, including without limitation
 // the rights to use, copy, modify, merge, publish, distribute, sublicense,
 // and/or sell copies of the Software, and to permit persons to whom the
 // Software is furnished to do so, subject to the following conditions:
-//
+// 
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-//
+// 
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
@@ -35,17 +35,17 @@ namespace SM.Media
         readonly ConfigurationEventArgs _configurationEventArgs;
         readonly IConfigurationSource _configurator;
         readonly Action<TsPesPacket> _packetHandler;
-        readonly IStreamSource _streamBuffer;
+        readonly IStreamSource _streamSource;
 
-        public MediaStream(IConfigurationSource configurator, IStreamSource streamBuffer, Action<TsPesPacket> packetHandler)
+        public MediaStream(IConfigurationSource configurator, IStreamSource streamSource, Action<TsPesPacket> packetHandler)
         {
-            _streamBuffer = streamBuffer;
+            _streamSource = streamSource;
             _configurator = configurator;
             _packetHandler = packetHandler;
 
             configurator.ConfigurationComplete += ConfiguratorOnConfigurationComplete;
 
-            _configurationEventArgs = new ConfigurationEventArgs(configurator, _streamBuffer);
+            _configurationEventArgs = new ConfigurationEventArgs(configurator, _streamSource);
         }
 
         #region IMediaParserMediaStream Members
@@ -56,7 +56,7 @@ namespace SM.Media
         {
             _configurator.ConfigurationComplete -= ConfiguratorOnConfigurationComplete;
 
-            using (_streamBuffer as IDisposable)
+            using (_streamSource as IDisposable)
             { }
         }
 

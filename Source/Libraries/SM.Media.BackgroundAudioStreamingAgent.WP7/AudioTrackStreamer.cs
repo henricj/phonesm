c7d493e1657afc841bde3d532bc37e3c0196ab9b
Mikael Koskinen <mijapkos@gmail.com>
2013-10-23 13:26:18 +0300

    Background audio support for WP8

---
+++ Source/Libraries/SM.Media.BackgroundAudioStreamingAgent.WP7/AudioTrackStreamer.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using Microsoft.Phone.BackgroundAudio;
+using SM.Media.Playlists;
+using SM.Media.Segments;
+using SM.Media.Utility;
+using SM.Media.Web;
+using SM.TsParser;
+
+namespace SM.Media.BackgroundAudioStreamingAgent
+{
+    /// <summary>
+    /// A background agent that performs per-track streaming for playback
+    /// </summary>
+    public class AudioTrackStreamer : AudioStreamingAgent, IMediaElementManager
+    {
+        private AudioStreamer currentStreamer;
+        private TsMediaManager tsMediaManager;
+
+        protected async override void OnBeginStreaming(AudioTrack track, AudioStreamer streamer)
+        {
+            GlobalPlatformServices.Default = new PlatformServices();
+            currentStreamer = streamer;
+
+            var httpClients = new HttpClients();
+            var segmentsFactory = new SegmentsFactory(httpClients);
+
+            var programManager = new ProgramManager(httpClients, segmentsFactory.CreateStreamSegments)
+                                     {
+                                         Playlists = new[] { new Uri(track.Tag) }
+                                     };
+
+            var programs = await programManager.LoadAsync();
+            var program = programs.Values.First();
+            var subProgram = program.SubPrograms.First();
+
+            var programClient = httpClients.CreatePlaylistClient(program.Url);
+            var playlist = new PlaylistSegmentManager(uri => new CachedWebRequest(uri, programClient), subProgram, segmentsFactory.CreateStreamSegments);
+            var segmentReaderManager = new SegmentReaderManager(new[] { playlist }, httpClients.CreateSegmentClient);
+
+            var tsMediaStreamSource = new TsMediaStreamSource();
+            tsMediaManager = new TsMediaManager(segmentReaderManager, this, tsMediaStreamSource, streams =>
+                                                                                                     {
+                                                                                                         var firstAudio = streams.Streams.First(x => x.StreamType.Contents == TsStreamType.StreamContents.Audio);
+
+                                                                                                         var others = streams.Streams.Where(x => x.Pid != firstAudio.Pid);
+                                                                                                         foreach (
+                                                                                                             var programStream in others)
+                                                                                                         {
+                                                                                                             programStream.BlockStream = true;
+                                                                                                         }
+                                                                                                     });
+
+            tsMediaManager.OnStateChange += TsMediaManagerOnOnStateChange;
+            tsMediaManager.Play();
+        }
+
+        private void TsMediaManagerOnOnStateChange(object sender, TsMediaManagerStateEventArgs tsMediaManagerStateEventArgs)
+        {
+            var message = string.Format("Media manager state in background agent: {0}, message: {1}", tsMediaManagerStateEventArgs.State,
+                                        tsMediaManagerStateEventArgs.Message);
+            Debug.WriteLine(message);
+        }
+
+
+        /// <summary>
+        /// Called when the agent request is getting cancelled
+        /// The call to base.OnCancel() is necessary to release the background streaming resources
+        /// </summary>
+        protected override void OnCancel()
+        {
+            base.OnCancel();
+        }
+
+        public Task SetSource(IMediaStreamSource source)
+        {
+            var mediaStreamSource = (MediaStreamSource)source;
+
+            currentStreamer.SetSource(mediaStreamSource);
+
+            return TplTaskExtensions.CompletedTask;
+        }
+
+        public Task Close()
+        {
+            NotifyComplete();
+            return TplTaskExtensions.CompletedTask;
+        }
+    }
+}
---
+++ Source/Phone/BackgroundAudio.Sample.WP7/MainPage.xaml.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+using Microsoft.Phone.BackgroundAudio;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace BackgroundAudio.Sample.WP7
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Timer for updating the UI
+        DispatcherTimer _timer;
+
+        // Indexes into the array of ApplicationBar.Buttons
+        const int playButton = 0;
+        const int pauseButton = 1;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void MainPage_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            // Initialize a timer to update the UI every half-second.
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromSeconds(0.5);
+            _timer.Tick += new EventHandler(UpdateState);
+
+            BackgroundAudioPlayer.Instance.PlayStateChanged += new EventHandler(Instance_PlayStateChanged);
+
+            if (BackgroundAudioPlayer.Instance.PlayerState == PlayState.Playing)
+            {
+                // If audio was already playing when the app was launched, update the UI.
+                positionIndicator.IsIndeterminate = false;
+                positionIndicator.Maximum = BackgroundAudioPlayer.Instance.Track.Duration.TotalSeconds;
+                UpdateButtons(false, true);
+                UpdateState(null, null);
+            }
+        }
+
+        /// <summary>
+        /// PlayStateChanged event handler.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void Instance_PlayStateChanged(object sender, EventArgs e)
+        {
+            switch (BackgroundAudioPlayer.Instance.PlayerState)
+            {
+                case PlayState.Playing:
+                    // Update the UI.
+                    positionIndicator.IsIndeterminate = false;
+                    positionIndicator.Maximum = BackgroundAudioPlayer.Instance.Track.Duration.TotalSeconds;
+                    UpdateButtons(false, true);
+                    UpdateState(null, null);
+
+                    // Start the timer for updating the UI.
+                    _timer.Start();
+                    break;
+
+                case PlayState.Paused:
+                    // Update the UI.
+                    UpdateButtons(true, false);
+                    UpdateState(null, null);
+
+                    // Stop the timer for updating the UI.
+                    _timer.Stop();
+                    break;
+            }
+        }
+
+
+        /// <summary>
+        /// Helper method to update the state of the ApplicationBar.Buttons
+        /// </summary>
+        /// <param name="playBtnEnabled"></param>
+        /// <param name="pauseBtnEnabled"></param>
+        void UpdateButtons(bool playBtnEnabled, bool pauseBtnEnabled)
+        {
+            // Set the IsEnabled state of the ApplicationBar.Buttons array
+            ((ApplicationBarIconButton)(ApplicationBar.Buttons[playButton])).IsEnabled = playBtnEnabled;
+            ((ApplicationBarIconButton)(ApplicationBar.Buttons[pauseButton])).IsEnabled = pauseBtnEnabled;
+        }
+
+
+        /// <summary>
+        /// Updates the status indicators including the State, Track title, 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UpdateState(object sender, EventArgs e)
+        {
+            txtState.Text = string.Format("State: {0}", BackgroundAudioPlayer.Instance.PlayerState);
+
+            if (BackgroundAudioPlayer.Instance.Track != null)
+            {
+                txtTrack.Text = string.Format("Track: {0}", BackgroundAudioPlayer.Instance.Track.Title);
+
+                // Set the current position on the ProgressBar.
+                positionIndicator.Value = BackgroundAudioPlayer.Instance.Position.TotalSeconds;
+
+                // Update the current playback position.
+                TimeSpan position = new TimeSpan();
+                position = BackgroundAudioPlayer.Instance.Position;
+                textPosition.Text = String.Format("{0:d2}:{1:d2}:{2:d2}", position.Hours, position.Minutes, position.Seconds);
+
+                // Update the time remaining digits.
+                TimeSpan timeRemaining = new TimeSpan();
+                timeRemaining = BackgroundAudioPlayer.Instance.Track.Duration - position;
+                textRemaining.Text = String.Format("-{0:d2}:{1:d2}:{2:d2}", timeRemaining.Hours, timeRemaining.Minutes, timeRemaining.Seconds);
+            }
+        }
+
+        /// <summary>
+        /// Click handler for the Play button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void playButton_Click(object sender, EventArgs e)
+        {
+            if (BackgroundAudioPlayer.Instance.Track == null)
+                BackgroundAudioPlayer.Instance.Track = new AudioTrack(null, null, null, null, null, "http://devimages.apple.com/iphone/samples/bipbop/bipbopall.m3u8", EnabledPlayerControls.All);
+
+            // Tell the backgound audio agent to play the current track.
+            BackgroundAudioPlayer.Instance.Play();
+        }
+
+
+        /// <summary>
+        /// Click handler for the Pause button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void pauseButton_Click(object sender, EventArgs e)
+        {
+            // Tell the backgound audio agent to pause the current track.
+            BackgroundAudioPlayer.Instance.Pause();
+        }
+    }
+}

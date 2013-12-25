9670cbff92d07e86ce004c2eb484611ff7edafa7
henric <software@henric.org>
2013-12-24 18:05:49 -0700

    Add a TaskCollector to keep track of otherwise fire-and-forget Tasks (only used during debugging).

---
+++ Source/Libraries/SM.Media/Utility/TaskCollector.cs
@@ -0,0 +1,92 @@
+// -----------------------------------------------------------------------
+//  <copyright file="TaskCollector.cs" company="Henric Jungheim">
+//  Copyright (c) 2012, 2013.
+//  <author>Henric Jungheim</author>
+//  </copyright>
+// -----------------------------------------------------------------------
+// Copyright (c) 2012, 2013 Henric Jungheim <software@henric.org>
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace SM.Media.Utility
+{
+    public class TaskCollector
+    {
+        public static readonly TaskCollector Default = new TaskCollector();
+        readonly object _lock = new object();
+        readonly Dictionary<Task, string> _tasks = new Dictionary<Task, string>();
+
+        [Conditional("DEBUG")]
+        public void Add(Task task, string description)
+        {
+            if (task.IsCompleted)
+            {
+                ReportException(task, description);
+
+                return;
+            }
+
+            lock (_lock)
+            {
+                Debug.Assert(!_tasks.ContainsKey(task));
+                _tasks[task] = description;
+            }
+
+            task.ContinueWith(Cleanup);
+        }
+
+        void Cleanup(Task task)
+        {
+            Debug.Assert(task.IsCompleted);
+
+            var wasRemoved = false;
+            string description;
+
+            lock (_lock)
+            {
+                if (_tasks.TryGetValue(task, out description))
+                    wasRemoved = _tasks.Remove(task);
+            }
+
+            Debug.Assert(wasRemoved, description ?? "No description");
+
+            ReportException(task, description);
+        }
+
+        static void ReportException(Task task, string description)
+        {
+            try
+            {
+                var ex = task.Exception;
+
+                if (null != ex)
+                    Debug.WriteLine("TaskCollector.Cleanup() task {0} failed: {1}", description, ex.Message);
+            }
+            catch (Exception ex2)
+            {
+                Debug.WriteLine("TaskCollector.Cleanup() cleanup of task {0} failed: {1}", description, ex2.Message);
+            }
+        }
+    }
+}

587e93f765a98b16839c7be7719c202518a2702e
henric <software@henric.org>
2014-02-08 06:28:50 -0700

    AacAudioSpecificConfig can be static.

--- Source/Libraries/SM.Media/AAC/AacAudioSpecificConfig.cs
+++ Source/Libraries/SM.Media/AAC/AacAudioSpecificConfig.cs
@@ -1,10 +1,10 @@
 // -----------------------------------------------------------------------
 //  <copyright file="AacAudioSpecificConfig.cs" company="Henric Jungheim">
-//  Copyright (c) 2012, 2013.
+//  Copyright (c) 2012-2014.
 //  <author>Henric Jungheim</author>
 //  </copyright>
 // -----------------------------------------------------------------------
-// Copyright (c) 2012, 2013 Henric Jungheim <software@henric.org>
+// Copyright (c) 2012-2014 Henric Jungheim <software@henric.org>
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a
 // copy of this software and associated documentation files (the "Software"),
@@ -28,7 +28,7 @@ using System.Diagnostics;
 
 namespace SM.Media.AAC
 {
-    public sealed class AacAudioSpecificConfig
+    public static class AacAudioSpecificConfig
     {
         /// <summary>
         ///     AAC Main isn't really used anymore.  Sometimes the encoder may

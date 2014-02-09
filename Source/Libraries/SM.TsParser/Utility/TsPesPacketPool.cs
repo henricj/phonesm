2524b54b16844590c8ff720006c7a26ea463dda7
henric <software@henric.org>
2014-02-09 12:17:12 -0700

    TsPesPacketPool should dispose its object pool.

--- Source/Libraries/SM.TsParser/Utility/TsPesPacketPool.cs
+++ Source/Libraries/SM.TsParser/Utility/TsPesPacketPool.cs
@@ -1,10 +1,10 @@
 // -----------------------------------------------------------------------
 //  <copyright file="TsPesPacketPool.cs" company="Henric Jungheim">
-//  Copyright (c) 2012, 2013.
+//  Copyright (c) 2012-2014.
 //  <author>Henric Jungheim</author>
 //  </copyright>
 // -----------------------------------------------------------------------
-// Copyright (c) 2012, 2013 Henric Jungheim <software@henric.org>
+// Copyright (c) 2012-2014 Henric Jungheim <software@henric.org>
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a
 // copy of this software and associated documentation files (the "Software"),
@@ -46,7 +46,8 @@ namespace SM.TsParser.Utility
 
         public void Dispose()
         {
-            Clear();
+            using (_packetPool)
+            { }
         }
 
         #endregion

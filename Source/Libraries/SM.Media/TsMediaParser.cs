a7083b730b0d2ac4fceedbaca9f3755b3d169ad7
henric <software@henric.org>
2014-02-16 06:39:30 -0700

    Update TsMediaParser to match recent changes. Make sure to run the configuration notification asynchronously to avoid a possible deadlock if open media fails.

--- Source/Libraries/SM.Media/TsMediaParser.cs
+++ Source/Libraries/SM.Media/TsMediaParser.cs
@@ -27,58 +27,52 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
+using SM.Media.Configuration;
 using SM.Media.MediaParser;
 using SM.Media.Pes;
 using SM.Media.Utility;
 using SM.TsParser;
+using SM.TsParser.Utility;
 
 namespace SM.Media
 {
     public sealed class TsMediaParser : IMediaParser
     {
-        #region Delegates
-
-        public delegate MediaStream PacketHandlerFactory(TsDecoder tsDecoder, uint pid, TsStreamType streamType, IStreamSource streamBuffer, Action<TsPesPacket> nextHandler);
-
-        #endregion
-
-        readonly BufferPool _bufferPool;
-        Action<IMediaParserMediaStream> _mediaParserStreamHandler;
+        readonly IBufferPool _bufferPool;
         readonly List<IMediaParserMediaStream> _mediaStreams = new List<IMediaParserMediaStream>();
         readonly object _mediaStreamsLock = new object();
-        readonly PesHandlers _pesHandlers;
-        Func<TsStreamType, Action<TsPesPacket>, StreamBuffer> _streamBufferFactory;
+        readonly IPesHandlers _pesHandlers;
         readonly List<Action<TimeSpan>> _timestampOffsetHandlers = new List<Action<TimeSpan>>();
-        readonly TsDecoder _tsDecoder;
+        readonly ITsDecoder _tsDecoder;
+        readonly ITsPesPacketPool _tsPesPacketPool;
         readonly ITsTimestamp _tsTimemestamp;
+        Func<TsStreamType, Action<TsPesPacket>, StreamBuffer> _streamBufferFactory;
+        int? _streamCount;
 
-        public TsMediaParser(ITsTimestamp tsTimemestamp)
+        public TsMediaParser(ITsDecoder tsDecoder, ITsPesPacketPool tsPesPacketPool, IBufferPool bufferPool, ITsTimestamp tsTimemestamp, IPesHandlers pesHandlers)
         {
-            if (tsTimemestamp == null)
+            if (null == tsDecoder)
+                throw new ArgumentNullException("tsDecoder");
+            if (null == tsPesPacketPool)
+                throw new ArgumentNullException("tsPesPacketPool");
+            if (null == bufferPool)
+                throw new ArgumentNullException("bufferPool");
+            if (null == tsTimemestamp)
                 throw new ArgumentNullException("tsTimemestamp");
+            if (null == pesHandlers)
+                throw new ArgumentNullException("pesHandlers");
 
+            _tsPesPacketPool = tsPesPacketPool;
+            _bufferPool = bufferPool;
+            _tsDecoder = tsDecoder;
             _tsTimemestamp = tsTimemestamp;
-            Func<uint, TsStreamType, Action<TsPesPacket>> handlerFactory;
-            {
-                var phf = new PesHandlerFactory();
-
-                RegisterHandlers(phf, TsMediaHandlerFactories.DefaultFactories);
-
-                handlerFactory = phf.CreateHandler;
-            }
-
-            _pesHandlers = new PesHandlers(handlerFactory);
-
-            _bufferPool = new BufferPool(new DefaultBufferPoolParameters
-                                         {
-                                             BaseSize = 5 * 64 * 1024,
-                                             Pools = 2
-                                         });
-
-            _tsDecoder = new TsDecoder(_bufferPool, _pesHandlers.GetPesHandler);
+            _pesHandlers = pesHandlers;
         }
 
-        public IMediaParserMediaStream[] MediaStreams
+        #region IMediaParser Members
+
+        public ICollection<IMediaParserMediaStream> MediaStreams
         {
             get
             {
@@ -89,18 +83,13 @@ namespace SM.Media
             }
         }
 
-        public TsDecoder Decoder
-        {
-            get { return _tsDecoder; }
-        }
-
-        #region IMediaParser Members
-
         public TimeSpan StartPosition
         {
             get { return _tsTimemestamp.StartPosition; }
             set { _tsTimemestamp.StartPosition = value; }
         }
+
+        public event EventHandler<ConfigurationEventArgs> ConfigurationComplete;
 
         public bool EnableProcessing
         {
@@ -114,33 +103,31 @@ namespace SM.Media
         /// <filterpriority>2</filterpriority>
         public void Dispose()
         {
-            using (_pesHandlers)
-            { }
-
             CleanupStreams();
-
-            using (_tsDecoder)
-            { }
-
-            _bufferPool.Dispose();
         }
 
-        public void Initialize(Func<TsStreamType, Action<TsPesPacket>, StreamBuffer> streamBufferFactory, Action<IMediaParserMediaStream> mediaParserStreamHandler,
-            Action<IProgramStreams> programStreamsHandler = null)
+        public void Initialize(Func<TsStreamType, Action<TsPesPacket>, StreamBuffer> streamBufferFactory, Action<IProgramStreams> programStreamsHandler = null)
         {
             if (streamBufferFactory == null)
                 throw new ArgumentNullException("streamBufferFactory");
 
             _streamBufferFactory = streamBufferFactory;
-            _mediaParserStreamHandler = mediaParserStreamHandler;
 
-            //Clear(Decoder.Initialize);
-            Decoder.Initialize(programStreamsHandler);
+            var handler = programStreamsHandler ?? DefaultProgramStreamsHandler;
+
+            programStreamsHandler = pss =>
+                                    {
+                                        handler(pss);
+
+                                        _streamCount = pss.Streams.Count(s => !s.BlockStream);
+                                    };
+
+            _tsDecoder.Initialize(CreatePacketizedElementaryStream, programStreamsHandler);
         }
 
         public void FlushBuffers()
         {
-            Decoder.FlushBuffers();
+            _tsDecoder.FlushBuffers();
             _tsTimemestamp.Flush();
         }
 
@@ -156,63 +143,91 @@ namespace SM.Media
 
         #endregion
 
+        static void DefaultProgramStreamsHandler(IProgramStreams pss)
+        {
+            var hasAudio = false;
+            var hasVideo = false;
+
+            foreach (var stream in pss.Streams)
+            {
+                switch (stream.StreamType.Contents)
+                {
+                    case TsStreamType.StreamContents.Audio:
+                        if (hasAudio)
+                            stream.BlockStream = true;
+                        else
+                            hasAudio = true;
+                        break;
+                    case TsStreamType.StreamContents.Video:
+                        if (hasVideo)
+                            stream.BlockStream = true;
+                        else
+                            hasVideo = true;
+                        break;
+                    default:
+                        stream.BlockStream = true;
+                        break;
+                }
+            }
+        }
+
         void CleanupStreams()
         {
-            IMediaParserMediaStream[] oldStreams;
-
             lock (_mediaStreamsLock)
             {
-                oldStreams = _mediaStreams.ToArray();
                 _mediaStreams.Clear();
             }
-
-            foreach (var ms in oldStreams)
-            {
-                using (ms)
-                { }
-            }
         }
 
-        void AddMediaStream(IMediaParserMediaStream mediaParserMediaStream)
+        void AddMediaStream(MediaStream mediaStream)
         {
             lock (_mediaStreamsLock)
             {
-                _mediaStreams.Add(mediaParserMediaStream);
+                _mediaStreams.Add(mediaStream);
             }
 
-            var h = _mediaParserStreamHandler;
+            CheckConfigurationComplete();
+        }
 
-            if (null == h)
+        void CheckConfigurationComplete()
+        {
+            var streams = MediaStreams;
+
+            if (!_streamCount.HasValue || _streamCount.Value != streams.Count)
                 return;
 
-            h(mediaParserMediaStream);
+            if (streams.Any(stream => null == stream.ConfigurationSource || !stream.ConfigurationSource.IsConfigured))
+                return;
+
+            FireConfigurationComplete();
         }
 
-        void RegisterHandlers(PesHandlerFactory pesHandlerFactory, IEnumerable<KeyValuePair<byte, PacketHandlerFactory>> factories)
+        void FireConfigurationComplete()
         {
-            foreach (var handlerFactory in factories)
-            {
-                var id = handlerFactory.Key;
-                var factory = handlerFactory.Value;
+            var cc = ConfigurationComplete;
 
-                pesHandlerFactory.RegisterHandlerFactory(id, (pid, streamType) => CreatePacketHandler(factory, pid, streamType));
-            }
+            if (null == cc)
+                return;
+
+            ConfigurationComplete = null;
+
+            var task = TaskEx.Run(() => cc(this, new ConfigurationEventArgs(null, null)));
+
+            TaskCollector.Default.Add(task, "TsMediaParser.FireConfigurationComplete()");
         }
 
-        Action<TsPesPacket> CreatePacketHandler(PacketHandlerFactory streamHandlerFactory, uint pid, TsStreamType streamType)
+        TsPacketizedElementaryStream CreatePacketizedElementaryStream(TsStreamType streamType, uint pid)
         {
-            var streamBuffer = _streamBufferFactory(streamType, _tsDecoder.PesPacketPool.FreePesPacket);
-
-            var localStreamBuffer = streamBuffer;
+            var streamBuffer = _streamBufferFactory(streamType, _tsPesPacketPool.FreePesPacket);
 
             lock (_mediaStreamsLock)
             {
-                _timestampOffsetHandlers.Add(ts => localStreamBuffer.TimestampOffset = ts);
+                _timestampOffsetHandlers.Add(ts => streamBuffer.TimestampOffset = ts);
             }
 
             var gotFirstPacket = false;
 
-            var ms = streamHandlerFactory(_tsDecoder, pid, streamType, streamBuffer,
+            var pesStreamHandler = _pesHandlers.GetPesHandler(streamType, pid,
                 packet =>
                 {
                     if (null != packet)
@@ -233,15 +248,32 @@ namespace SM.Media
 
                         gotFirstPacket = true;
 
-                        Debug.Assert(packet.PresentationTimestamp >= StartPosition, string.Format("packet.Timestamp >= StartPosition: {0} >= {1} is {2}", packet.PresentationTimestamp, StartPosition, packet.PresentationTimestamp >= StartPosition));
+                        Debug.Assert(packet.PresentationTimestamp >= StartPosition, String.Format("packet.Timestamp >= StartPosition: {0} >= {1} is {2}", packet.PresentationTimestamp, StartPosition, packet.PresentationTimestamp >= StartPosition));
                     }
 
                     streamBuffer.Enqueue(packet);
                 });
 
+            var pes = new TsPacketizedElementaryStream(_bufferPool, _tsPesPacketPool, pesStreamHandler.PacketHandler, streamType, pid);
+
+            var configurator = pesStreamHandler.Configurator;
+
+            EventHandler configuratorOnConfigurationComplete = null;
+
+            configuratorOnConfigurationComplete = (o, e) =>
+                                                  {
+                                                      configurator.ConfigurationComplete -= configuratorOnConfigurationComplete;
+
+                                                      CheckConfigurationComplete();
+                                                  };
+
+            configurator.ConfigurationComplete += configuratorOnConfigurationComplete;
+
+            var ms = new MediaStream(configurator, streamBuffer);
+
             AddMediaStream(ms);
 
-            return ms.PacketHandler;
+            return pes;
         }
     }
 }

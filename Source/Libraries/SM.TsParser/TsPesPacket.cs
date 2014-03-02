b98085cb94ca4c302049c3485faf96e1d3fea0cd
henric <software@henric.org>
2014-03-02 07:45:45 -0700

    Add a .ToString() to TsPesPacket.

--- Source/Libraries/SM.TsParser/TsPesPacket.cs
+++ Source/Libraries/SM.TsParser/TsPesPacket.cs
@@ -1,10 +1,10 @@
 // -----------------------------------------------------------------------
 //  <copyright file="TsPesPacket.cs" company="Henric Jungheim">
-//  Copyright (c) 2012, 2013.
+//  Copyright (c) 2012-2014.
 //  <author>Henric Jungheim</author>
 //  </copyright>
 // -----------------------------------------------------------------------
-// Copyright (c) 2012, 2013 Henric Jungheim <software@henric.org>
+// Copyright (c) 2012-2014 Henric Jungheim <software@henric.org>
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a
 // copy of this software and associated documentation files (the "Software"),
@@ -57,5 +57,16 @@ namespace SM.TsParser
         static int _packetCount;
         public readonly int PacketId = Interlocked.Increment(ref _packetCount);
 #endif
+
+        public override string ToString()
+        {
+#if DEBUG
+            return string.Format("Packet({0}) index {1} length {2} duration {3} timestamp {4}/{5} buffer {6}",
+                PacketId, Index, Length, Duration, PresentationTimestamp, DecodeTimestamp, BufferEntry);
+#else
+            return string.Format("Packet index {0} length {1} duration {2} timestamp {3}/{4} buffer {5}",
+                Index, Length, Duration, PresentationTimestamp, DecodeTimestamp, BufferEntry);
+#endif
+        }
     }
 }

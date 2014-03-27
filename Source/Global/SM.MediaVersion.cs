9422b3516d474bbc4b287fe3c4fdec9ba4945cc8
henric <software@henric.org>
2014-03-26 20:44:45 -0700

    Avoid confusion with binary download.

--- Source/Global/SM.MediaVersion.cs
+++ Source/Global/SM.MediaVersion.cs
@@ -37,6 +37,6 @@ using System.Reflection;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.2.2.0")]
-[assembly: AssemblyFileVersion("1.2.2.0")]
-[assembly: AssemblyInformationalVersion("1.2.2 beta")]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyInformationalVersion("1.3.0 master")]

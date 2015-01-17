bf5e6fb1bdff0228883e5e9651bf1255231b6259
henric <software@henric.org>
2015-01-16 19:51:49 -0700

    Fix XML comment warning.

--- Source/Libraries/SM.Media/Segments/ISegment.cs
+++ Source/Libraries/SM.Media/Segments/ISegment.cs
@@ -1,10 +1,10 @@
 // -----------------------------------------------------------------------
 //  <copyright file="ISegment.cs" company="Henric Jungheim">
-//  Copyright (c) 2012-2014.
+//  Copyright (c) 2012-2015.
 //  <author>Henric Jungheim</author>
 //  </copyright>
 // -----------------------------------------------------------------------
-// Copyright (c) 2012-2014 Henric Jungheim <software@henric.org>
+// Copyright (c) 2012-2015 Henric Jungheim <software@henric.org>
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a
 // copy of this software and associated documentation files (the "Software"),
@@ -50,6 +50,7 @@ namespace SM.Media.Segments
         ///     stripping headers from .mp3 or decrypting #EXT-X-KEY AES-128 segments).
         /// </summary>
         /// <param name="stream"></param>
+        /// <param name="cancellationToken"></param>
         /// <returns>null if no filter is required</returns>
         Task<Stream> CreateFilterAsync(Stream stream, CancellationToken cancellationToken);
     }

439c147fbf6dd75206ff57371f3f364bf62d5a4f
henric <software@henric.org>
2015-01-17 00:59:21 -0700

    Unbreak HttpConnection on Silverlight.

--- Source/Libraries/SM.Media.Platform.Silverlight/HttpConnectionModule.cs
+++ Source/Libraries/SM.Media.Platform.Silverlight/HttpConnectionModule.cs
@@ -1,10 +1,10 @@
 // -----------------------------------------------------------------------
 //  <copyright file="HttpConnectionModule.cs" company="Henric Jungheim">
-//  Copyright (c) 2012-2014.
+//  Copyright (c) 2012-2015.
 //  <author>Henric Jungheim</author>
 //  </copyright>
 // -----------------------------------------------------------------------
-// Copyright (c) 2012-2014 Henric Jungheim <software@henric.org>
+// Copyright (c) 2012-2015 Henric Jungheim <software@henric.org>
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a
 // copy of this software and associated documentation files (the "Software"),
@@ -38,6 +38,8 @@ namespace SM.Media
             builder.RegisterType<SocketWrapper>().As<ISocket>().ExternallyOwned();
 
             builder.RegisterType<HttpConnection>().As<IHttpConnection>().ExternallyOwned();
+            builder.RegisterType<HttpConnectionFactory>().As<IHttpConnectionFactory>().SingleInstance();
+
             builder.RegisterType<HttpConnectionWebReaderManager>().As<IWebReaderManager>().SingleInstance();
 
             builder.RegisterType<HttpEncoding>().As<IHttpEncoding>().SingleInstance();

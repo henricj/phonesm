d1bc482b750d93d99fd8dec5e9fd8d346a7c81a9
henric <software@henric.org>
2015-02-19 14:03:02 -0700

    Return a cancelled task if the token is cancelled. (This is what unit tests are for...)

--- Source/Libraries/SM.Media/Utility/CancellationTokenExtensions.cs
+++ Source/Libraries/SM.Media/Utility/CancellationTokenExtensions.cs
@@ -1,10 +1,10 @@
 // -----------------------------------------------------------------------
 //  <copyright file="CancellationTokenExtensions.cs" company="Henric Jungheim">
-//  Copyright (c) 2012-2014.
+//  Copyright (c) 2012-2015.
 //  <author>Henric Jungheim</author>
 //  </copyright>
 // -----------------------------------------------------------------------
-// Copyright (c) 2012-2014 Henric Jungheim <software@henric.org>
+// Copyright (c) 2012-2015 Henric Jungheim <software@henric.org>
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a
 // copy of this software and associated documentation files (the "Software"),
@@ -60,7 +60,7 @@ namespace SM.Media.Utility
             if (!cancellationToken.CanBeCanceled)
                 return PendingTask;
 
-            if (!cancellationToken.IsCancellationRequested)
+            if (cancellationToken.IsCancellationRequested)
                 return CancelledTask;
 
             return WaitAsync(cancellationToken);

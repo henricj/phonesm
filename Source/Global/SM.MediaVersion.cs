e775a98d993825f74f461e871f3e0c3e9bfc0d3a
henric <software@henric.org>
2015-04-12 18:05:36 -0700

    Bump version.  (Removing SM.TsParser was not a minor change.)

--- Source/Global/SM.MediaVersion.cs
+++ Source/Global/SM.MediaVersion.cs
@@ -37,6 +37,6 @@ using System.Reflection;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.4.8.0")]
-[assembly: AssemblyFileVersion("1.4.8.0")]
-[assembly: AssemblyInformationalVersion("1.4.8 master")]
+[assembly: AssemblyVersion("1.5.0.0")]
+[assembly: AssemblyFileVersion("1.5.0.0")]
+[assembly: AssemblyInformationalVersion("1.5.0 master")]

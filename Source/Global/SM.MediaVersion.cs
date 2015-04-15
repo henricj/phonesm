d9ab0dd9bc9a20b69015bd10f6132a0483e562ae
henric <software@henric.org>
2015-04-15 00:58:29 -0700

    Bump version.

--- Source/Global/SM.MediaVersion.cs
+++ Source/Global/SM.MediaVersion.cs
@@ -37,6 +37,6 @@ using System.Reflection;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.5.0.0")]
-[assembly: AssemblyFileVersion("1.5.0.0")]
-[assembly: AssemblyInformationalVersion("1.5.0 master")]
+[assembly: AssemblyVersion("1.5.1.0")]
+[assembly: AssemblyFileVersion("1.5.1.0")]
+[assembly: AssemblyInformationalVersion("1.5.1 alpha")]
